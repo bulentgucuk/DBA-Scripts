b454af554f88c00af06954562afe76d852b384b6
Bulent Gucuk <bgucuk@ssbinfo.com>
2018-01-24 12:17:21 -0700

    Script changes for finding the size of the tables/indexes

--- Find Table and Index Size In Database.sql
+++ Find Table and Index Size In Database.sql
@@ -14,10 +14,13 @@ SELECT
 		WHEN MAX(ps.row_count) = 0 THEN 0
 		ELSE (8 * 1024* SUM(ps.reserved_page_count)) / NULLIF(MAX(ps.row_count), 0)
 		END AS 'Bytes/Row'
+	, p.Data_compression_desc
 FROM	sys.dm_db_partition_stats AS ps
 	INNER JOIN sys.indexes AS i ON ps.object_id = i.object_id and ps.index_id = i.index_id
 	INNER JOIN sys.tables AS t ON i.object_id = t.object_id
 	INNER JOIN sys.schemas AS s ON t.schema_id = s.schema_id
+	INNER JOIN sys.partitions as p ON p.index_id = i.index_id and p.object_id = t.object_id
 WHERE	t.is_ms_shipped = 0
-GROUP BY s.name, t.Name, i.Name, i.index_id, t.create_date
-ORDER BY SchemaName, TableName, i.index_id, SpaceInMB DESC;
+GROUP BY s.name, t.Name, i.Name, i.index_id, t.create_date, p.Data_compression_desc
+ORDER BY SchemaName, TableName, i.index_id, SpaceInMB DESC
+OPTION(RECOMPILE);
