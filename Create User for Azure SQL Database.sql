da3cf5835c61179d92f66cdb7f8ec291688c17ba
Bulent <bulentgucuk@gmail.com>
2019-02-13 06:34:39 -0700

    Changes
    New Changes

--- Create User for Azure SQL Database.sql
+++ Create User for Azure SQL Database.sql
@@ -1,6 +1,6 @@
 /****** Object:  SQL User For Azure SQL Database Create Script Date: 2/9/2018 1:14:55 PM ******/
 DECLARE	@UserName SYSNAME = 'ssb_azure_dba'
-	, @Password NVARCHAR(128) = ''
+	, @Password NVARCHAR(128) = 'jC73dYDu82BmwYNyTq'
 	, @RoleName VARCHAR(64) = 'db_owner'
 	, @str NVARCHAR(1024);
 
@@ -17,7 +17,6 @@ PRINT @str;
 EXEC sp_executesql @stmt = @str;
 
 GO
-
 
 /****** Object:  SQL User For Azure SQL Database Create Script Date: 2/9/2018 1:14:55 PM ******/
 DECLARE	@UserName SYSNAME = 'jmalone@ssbinfo.com'
--- Find Internal and External Fragmentation.sql
+++ Find Internal and External Fragmentation.sql
@@ -1,27 +1,15 @@
-SELECT
-	  DB_NAME() AS 'DatabaseName'
-	, s.name AS 'SchemaName'
-	, t.Name AS 'TableName'
-	, t.create_date AS 'CreateDate'
-	, CASE
-		WHEN I.index_id = 0 THEN 'HEAP'
-		ELSE i.Name
-		END AS 'IndexName'
-	, i.index_id AS 'IndexId'
-	, MAX(ps.row_count) AS 'RowCount'
-	, SUM(ps.reserved_page_count) * 8.0 / (1024) as 'SpaceInMB'
-	, (SUM(ps.reserved_page_count) * 8.0 / (1024))/ 1024 as 'SpaceInGB'
-	, CASE
-		WHEN MAX(ps.row_count) = 0 THEN 0
-		ELSE (8 * 1024* SUM(ps.reserved_page_count)) / NULLIF(MAX(ps.row_count), 0)
-		END AS 'Bytes/Row'
-	, p.Data_compression_desc
-FROM	sys.dm_db_partition_stats AS ps
-	INNER JOIN sys.indexes AS i ON ps.object_id = i.object_id and ps.index_id = i.index_id
-	INNER JOIN sys.tables AS t ON i.object_id = t.object_id
-	INNER JOIN sys.schemas AS s ON t.schema_id = s.schema_id
-	INNER JOIN sys.partitions as p ON p.index_id = i.index_id and p.object_id = t.object_id
-WHERE	t.is_ms_shipped = 0
-GROUP BY s.name, t.Name, i.Name, i.index_id, t.create_date, p.Data_compression_desc
-ORDER BY SpaceInMB DESC
-OPTION(RECOMPILE);
+USE Cowboys_Reporting
+GO
+SELECT DB_NAME(database_id) AS 'Database Name'
+,[object_id]
+,Object_Name([object_id])
+,index_id
+,avg_fragmentation_in_percent AS 'External Fragmentation'
+,avg_page_space_used_in_percent AS 'Internal Fragmentation' 
+,*
+FROM sys.dm_db_index_physical_stats(db_id(),OBJECT_ID('prodcopystg.Account'),NULL,NULL,'DETAILED')
+--WHERE (avg_fragmentation_in_percent > 10 
+--OR 
+--avg_page_space_used_in_percent < 75)
+--AND avg_page_space_used_in_percent <> 0
+--AND page_count > 8;
