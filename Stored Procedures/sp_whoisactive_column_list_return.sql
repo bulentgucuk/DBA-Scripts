7bca5a537bcb052a272109f97f59a25f7d3fffdb
Bulent <bulentgucuk@gmail.com>
2019-02-14 15:40:00 -0700

    sp_whoisactive with specific columns
    sp_whoisactive with specific columns

--- Stored Procedures/sp_whoisactive_column_list_return.sql
+++ Stored Procedures/sp_whoisactive_column_list_return.sql
@@ -5,5 +5,6 @@ exec sp_WhoIsActive
 	
 
 	
-exec sp_WhoIsActive
+exec sp_WhoIsActive @get_outer_command = 1,
 @output_column_list  = '[dd%],[session_id],[blocking%],[sql_text],[sql_command],[login_name],[wait_info],[host_name],[database_name],[program_name],[tran_log%],[cpu%],[temp%],[block%],[reads%],[writes%],[context%],[physical%],[start%]'
+
