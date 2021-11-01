bb641148d47796bd8bf10c0fbc7741543a6c16db
bulentgucuk <bulentgucuk@gmail.com>
2021-11-01 11:57:43 -0600

    Move command to above line

--- PowerShell/_AAA;.ps1
+++ PowerShell/_AAA;.ps1
@@ -3,16 +3,14 @@ choco list --local-only;
 choco outdated;
 <#
 choco uninstall dbatools --all-versions -y
-
 choco install dbatools -y
 
 choco upgrade azcopy10 -y
+choco upgrade chocolatey -y
 choco upgrade dbatools -y
 choco upgrade git -Y
 choco upgrade roundhouse -y
 choco upgrade sysinternals -y
-choco upgrade chocolatey -y
-
 
 choco search dbaclone --remote
 choco --help
